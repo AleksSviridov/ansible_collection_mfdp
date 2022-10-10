ee12ea7fd70c230c0034299df68ff2543e678149
AleksSviridov <alasviridov@gmail.com>
2022-10-10 23:34:51 +0300

    Remove ignored files

--- rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /storage/cifs/dev/sviridov/dataprotector
-ansible-galaxy collection build --force
-ansible-galaxy collection install sviridov-dataprotector-1.0.0.tar.gz --force
-ansible-playbook tests/integration/targets/mfdp_users/main.yml
